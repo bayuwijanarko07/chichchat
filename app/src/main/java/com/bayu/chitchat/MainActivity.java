c36e3190a716d52bc703217549d697cce060f5c2
Bayu Wijanarko <bayuwijanarko07@gmail.com>
2019-07-25 08:59:06 +0700

    versi baru

--- app/src/main/java/com/bayu/chitchat/MainActivity.java
+++ app/src/main/java/com/bayu/chitchat/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,6 +19,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -28,18 +30,23 @@ import java.util.Map;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+
     private Button add_room;
     private EditText room_name;
     private ListView listView;
     private String name;
-    private DatabaseReference root = FirebaseDatabase.getInstance().getReference().getRoot();
+
+    private DatabaseReference dbNode = FirebaseDatabase.getInstance().getReference().getRoot();
+    private Query Room = dbNode.child("pie");
 
     private ArrayAdapter<String> arrayAdapter;
     private ArrayList<String> list_of_rooms = new ArrayList();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         add_room = (Button)findViewById(R.id.btnAdd_room);
         room_name = (EditText)findViewById(R.id.etNeme_room);
         listView = (ListView)findViewById(R.id.listView);
@@ -48,17 +55,33 @@ public class MainActivity extends AppCompatActivity {
         listView.setAdapter(arrayAdapter);
 
         request_user_name();
+
+        Room.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot appleSnapshot: dataSnapshot.getChildren()) {
+                    appleSnapshot.getRef().removeValue();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
         add_room.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Map<String,Object> map = new HashMap<String,Object>();
                 map.put(room_name.getText().toString(),"");
-                root.updateChildren(map);
+                dbNode.updateChildren(map);
 
             }
         });
-        root.addValueEventListener(new ValueEventListener() {
+
+        dbNode.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Set<String> set = new HashSet<String>();
@@ -90,11 +113,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+//       listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//           @Override
+//           public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long room) {
+//
+//               Map<String,Object> mapObject = (Map<String, Object>) parent.getAdapter().getItem(position);
+//
+//               int root =  (int) mapObject.get("room_name");
+//               konfirmHapus(root);
+//               return false;
+//           }
+//       });
+
     }
 
     private void request_user_name() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Enter Name");
+        builder.setTitle("Username");
         final EditText input_field = new EditText(this);
         builder.setView(input_field);
         builder.setPositiveButton("OK ", new DialogInterface.OnClickListener() {
@@ -112,4 +147,19 @@ public class MainActivity extends AppCompatActivity {
         });
         builder.show();
     }
+
+//    void konfirmHapus(final int room_name){
+//        new AlertDialog.Builder(this)
+//                .setTitle("Hapus Data ini?")
+//                .setMessage("Apakah Anda yakin ingin menghapus data ini?")
+//                .setIcon(android.R.drawable.ic_dialog_alert)
+//                .setPositiveButton("Hapus",
+//                        new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog,
+//                                                int whichButton) {
+//                            }
+//                        })
+//                .setNegativeButton("Jangan", null)
+//                .show();
+//    }
 }
